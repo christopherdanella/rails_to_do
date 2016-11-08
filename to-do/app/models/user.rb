a8f2fc30bf26210576ab43bcf9d77625deabfdb3
Christopher Danella <christopher.danella@gmail.com>
2016-11-07 21:26:16 -0500

    set up user authentication

---
+++ to-do/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  has_many :tasks
+
+  has_secure_password
+end
