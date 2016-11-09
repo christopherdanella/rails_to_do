686fd32f5581f7e756cca163a1eb4a6e46365c5f
Christopher Danella <christopher.danella@gmail.com>
2016-11-08 19:57:00 -0500

    finalize user authentication

--- to-do/app/controllers/sessions_controller.rb
+++ to-do/app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.user_id
+    @user = User.find_by(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
